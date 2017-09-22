0a9a95ceff6b51662b627e79db9356ac582de865
Arthur Leung <arcyleung+github@gmail.com>
2017-09-22 19:33:25 -0400

    Added side nav

--- app/src/main/java/com/example/arthurl/mobooru/Main.java
+++ app/src/main/java/com/example/arthurl/mobooru/Main.java
@@ -14,14 +14,18 @@ import android.graphics.BitmapFactory;
 import android.graphics.Point;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.preference.PreferenceManager;
+import android.support.annotation.RequiresApi;
 import android.util.Log;
 import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -46,18 +50,34 @@ import org.jsoup.select.Elements;
 
 public class Main extends Activity {
 
-    int pageSize = 30;
-    private StaggeredGridView sgv;
-    private DataAdapter adapter;
-    public ArrayList<Data> datas = new ArrayList<Data>();
-
-
     final String verstring = "MoBooru v. 0.2a";
-
+    public ArrayList<Data> datas = new ArrayList<Data>();
+    int pageSize = 30;
     String mainsite = "https://redditbooru.com";
     URL url1;
-
     Display display;
+    int screenWidth = 0;
+    int screenHeight = 0;
+    int bitmapWidth = 0;
+    int bitmapHeight = 0;
+    // DEFAULT SETTINGS
+    String favstring = "";
+    Boolean showNsfw = false;
+    String s1 = "https://redditbooru.com/images/?sources=" + favstring + "&afterDate=";
+    long lastTime;
+    Document doc;
+    Elements redditSubs;
+    ArrayList<Sub> subsList = new ArrayList<Sub>();
+    String catJSONs = "";
+    JSONArray catJSONa;
+    LoadJSONasyncInit runner;
+    JSONArray jsonObjs;
+    LoadMorePhotos lm;
+    int current_page = 1;
+    int currentScrollPos = 0;
+    Boolean loadingMore = true;
+    private StaggeredGridView sgv;
+    private DataAdapter adapter;
 
     private static Point getDisplaySize(final Display display) {
         final Point point = new Point();
@@ -70,36 +90,12 @@ public class Main extends Activity {
         return point;
     }
 
-    int screenWidth = 0;
-    int screenHeight = 0;
-    int bitmapWidth = 0;
-    int bitmapHeight = 0;
-
-    // DEFAULT SETTINGS
-    String favstring = "";
-    Boolean showNsfw = false;
-
-    String s1 = "https://redditbooru.com/images/?sources=" + favstring + "&afterDate=";
-    long lastTime;
-    Document doc;
-    Elements redditSubs;
-    ArrayList<Sub> subsList = new ArrayList<Sub>();
-    String catJSONs = "";
-    JSONArray catJSONa;
-    LoadJSONasyncInit runner;
-    JSONArray jsonObjs;
-    LoadMorePhotos lm;
-
-    int current_page = 1;
-    int currentScrollPos = 0;
-    Boolean loadingMore = true;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
         favstring = prefs.getString("FAV_SUBS", "" + R.string.defaultsub).replaceAll(",", "%2C");
         showNsfw = prefs.getBoolean("SHOW_NSFW", false);
@@ -133,41 +129,38 @@ public class Main extends Activity {
         }
 
         sgv.setOnItemClickListener(new StaggeredGridView.OnItemClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
 
-                Toast.makeText(getActivity(), "Item Clicked: " + position, Toast.LENGTH_SHORT).show();
                 ImageView imView = new ImageView(getActivity());
-                try{
+                Dialog dialog = new Dialog(getActivity());
+                try {
 
                     Bitmap img = new DownloadImage(imView).execute(datas.get(position).imgUrl).get();
                     bitmapWidth = img.getWidth();
                     bitmapHeight = img.getHeight();
 
                     BitmapDrawable resizedBitmap = new BitmapDrawable(getActivity().getResources(), Bitmap.createScaledBitmap(img, bitmapWidth, bitmapHeight, false));
-                    while(bitmapHeight > (screenHeight - 250) || bitmapWidth > (screenWidth - 250)) {
+                    while (bitmapHeight > (screenHeight - 250) || bitmapWidth > (screenWidth - 250)) {
                         bitmapHeight = bitmapHeight / 2;
                         bitmapWidth = bitmapWidth / 2;
                     }
 
-                    Dialog dialog = new Dialog(getActivity());
+
                     dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
                     dialog.setContentView(R.layout.popup_imgview);
 
                     ImageView image = (ImageView) dialog.findViewById(R.id.imageview);
-
                     image.setBackground(resizedBitmap);
 
                     dialog.getWindow().setBackgroundDrawable(null);
-                    dialog.getWindow().addFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND);
+                    dialog.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
                     dialog.show();
 
-                } catch (Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
-
-//                finish();
-
             }
         });
     }
@@ -187,22 +180,99 @@ public class Main extends Activity {
         // Deserialize API response and then construct new objects to append to the adapter
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+
     public Context getActivity() {
         return this;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public ArrayList<Data> addToArry(JSONArray ja) {
+        if (loadingMore) {
+
+            //IMPLEMENT STOP LOADING ONCE ARRAYSIZE < PAGESIZE
+            if (ja == null) {
+                loadingMore = false;
+            } else {
+                if (ja.length() < pageSize) {
+                    pageSize = ja.length();
+                }
+                for (int i = 0; i < pageSize; i++) {
+                    Data data = new Data();
+                    try {
+                        data.thumbImgUrl = "https://redditbooru.com" + ja.getJSONObject(i).getString("thumb") + "_300_300.jpg";
+                        data.imgUrl = ja.getJSONObject(i).getString("cdnUrl");
+                        data.width = ja.getJSONObject(i).getInt("width");
+                        data.height = ja.getJSONObject(i).getInt("height");
+                        data.nsfw = ja.getJSONObject(i).getBoolean("nsfw");
+                        data.title = ja.getJSONObject(i).getString("title");
+                        data.desc = ja.getJSONObject(i).getString("sourceUrl");
+                        data.rat = data.width / data.height;
+                        if (i == pageSize - 1) {
+                            lastTime = Long.parseLong(ja.getJSONObject(i).getString("dateCreated"));
+                            System.out.println(lastTime);
+                        }
+                    } catch (Exception e) {
+                        System.out.println("JSON parse failed2");
+                        e.printStackTrace();
+                    }
+                    if (data.desc.equals("null")) {
+                        data.desc = "";
+                    }
+                    if (data.thumbImgUrl.equals("null")) {
+                        data.thumbImgUrl = "";
+                    }
+                    datas.add(data);
+                }
+            }
+        }
+        return datas;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                startActivity(new Intent(Main.this, Settings_subs.class).putExtra("arylst", subsList));
+                return true;
+            case R.id.action_about:
+                new AlertDialog.Builder(this)
+                        .setTitle("About")
+                        .setMessage("Author: pspkazy\nSite: http://github.com/pspkazy\n2014-2015")
+                        .setNegativeButton("Back", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // do nothing
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private class DownloadImage extends AsyncTask<String, Void, Bitmap> {
         ImageView bmImage;
         ProgressDialog pDialog;
 
-        protected void onPreExecute(){
+        public DownloadImage(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected void onPreExecute() {
             pDialog = new ProgressDialog(getActivity());
             pDialog.setMessage("Downloading...");
             pDialog.show();
-        }
-
-        public DownloadImage(ImageView bmImage) {
-            this.bmImage = bmImage;
         }
 
         protected Bitmap doInBackground(String... urls) {
@@ -336,78 +406,4 @@ public class Main extends Activity {
         }
 
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-
-    public ArrayList<Data> addToArry(JSONArray ja) {
-        if (loadingMore) {
-
-            //IMPLEMENT STOP LOADING ONCE ARRAYSIZE < PAGESIZE
-            if (ja == null) {
-                loadingMore = false;
-            } else {
-                if (ja.length() < pageSize) {
-                    pageSize = ja.length();
-                }
-                for (int i = 0; i < pageSize; i++) {
-                    Data data = new Data();
-                    try {
-                        data.thumbImgUrl = "https://redditbooru.com" + ja.getJSONObject(i).getString("thumb") + "_300_300.jpg";
-                        data.imgUrl = ja.getJSONObject(i).getString("cdnUrl");
-                        data.width = ja.getJSONObject(i).getInt("width");
-                        data.height = ja.getJSONObject(i).getInt("height");
-                        data.nsfw = ja.getJSONObject(i).getBoolean("nsfw");
-                        data.title = ja.getJSONObject(i).getString("title");
-                        data.desc = ja.getJSONObject(i).getString("sourceUrl");
-                        data.rat = data.width / data.height;
-                        if (i == pageSize - 1) {
-                            lastTime = Long.parseLong(ja.getJSONObject(i).getString("dateCreated"));
-                            System.out.println(lastTime);
-                        }
-                    } catch (Exception e) {
-                        System.out.println("JSON parse failed2");
-                        e.printStackTrace();
-                    }
-                    if (data.desc.equals("null")) {
-                        data.desc = "";
-                    }
-                    if (data.thumbImgUrl.equals("null")) {
-                        data.thumbImgUrl = "";
-                    }
-                    datas.add(data);
-                }
-            }
-        }
-        return datas;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                startActivity(new Intent(Main.this, Settings_subs.class).putExtra("arylst", subsList));
-                return true;
-            case R.id.action_about:
-                new AlertDialog.Builder(this)
-                        .setTitle("About")
-                        .setMessage("Author: pspkazy\nSite: http://github.com/pspkazy\n2014-2015")
-                        .setNegativeButton("Back", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                // do nothing
-                            }
-                        })
-                        .setIcon(android.R.drawable.ic_dialog_alert)
-                        .show();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
 }
